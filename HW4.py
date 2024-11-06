35403ebae6aa6a02cd01408df969d8e46f35d8d1
mrone <mroneill30@gmail.com>
2024-11-06 13:36:46 -0800

    2

--- HW4.py
+++ HW4.py
@@ -4,8 +4,6 @@ from sklearn.model_selection import KFold
 
 # Task 1
 
-learning_rate = 0.05
-
 # Data collection
 df = pd.read_csv("MNIST_CV.csv")
 
@@ -16,24 +14,35 @@ y = df.iloc[:, 0]
 def Sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-kf = KFold(n_splits=10)
+def logistic_regression(X_training, y_training, learning_rate = 0.01, iterations=1000):
+    m, n = X.shape # m-rows, n-columns
+    coefficient = np.zeros(n) # init coefficient array
+    for i in range(iterations):
+        logistic = Sigmoid(X_training.dot(coefficient.T))
+        costp = (-logistic + np.squeeze(y_training).T.dot(X_training)) ##gradient calc (direction)
+        y_training = np.squeeze(y_training)
+        coefficient = coefficient + learning_rate * costp
+    return coefficient
 
-for i, (train_index, test_index) in enumerate(kf.split(X)):
-    # print(f"Fold {i}")
-    # print(f"  Train: index={train_index}")
-    # print(f"  Test:  index={test_index}")
+def k_fold_cross_validation(X, y, k, learning_rate = 0.01, iterations=1000):
+    kf = KFold(n_splits=k)
 
-    logistic = Sigmoid(X_training.dot(coefficient.T))
-    costp = (-logistic + np.squeeze(y_training).T.dot(X_training)) ##gradient calc (direction)
-    y_training = np.squeeze(y_training)
-    coefficient = coefficient + learning_rate * costp
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
+        print(f"Fold {i}")
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-    # likelihood function y.log(p(xi) - ((1-y)(log(1-p(xi)))))
-    lw1 = (y_training * np.log(logistic))
+        coefficient = logistic_regression(X_train, y_train, learning_rate, iterations)
 
-    lw2 = ((1-y_training) * np.log(1-logistic))
-    costv = +lw1 + lw2
 
-    # mean of function
-    costf = np.mean(costv)
-    cost.append(costf)
+        
+
+# likelihood function y.log(p(xi) - ((1-y)(log(1-p(xi)))))
+lw1 = (y_training * np.log(logistic))
+
+lw2 = ((1-y_training) * np.log(1-logistic))
+costv = +lw1 + lw2
+
+# mean of function
+costf = np.mean(costv)
+cost.append(costf)
